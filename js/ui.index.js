2e0c3b36602045d73f1dfaa5ac093faf834736d3
Dave Ward <dave@encosia.com>
2014-02-02 14:08:56 -0500

    Added more example fields and support for checkbox groups
    Ugh, spacing...

--- js/ui.index.js
+++ js/ui.index.js
@@ -1,18 +1,33 @@
 $('#submit-big-form').on('click', function(evt) {
-    evt.preventDefault();
+  evt.preventDefault();
 
-    var $fields = $('#big-form').find('input, select, textarea').not(':submit');
+  var $fields = $('#big-form').find('input, select, textarea').not(':submit');
 
-    var formData = { };
+  var formData = {};
 
-    $fields.each(function() {
-        // Only checked radio buttons should contribute to the object.
-        if (this.type === 'radio' && !this.checked)
-            return;
+  $fields.each(function() {
+    // Only checked radio buttons should contribute to the object.
+    if (this.type === 'radio' && !this.checked)
+      return;
 
-        // Else
-        formData[this.name] = $(this).val();
-    });
+    // For checkbox groups, create an array of selected values.
+    if (this.type === 'checkbox') {
+      if (this.checked) {
+        // Ensure that this checkbox group's array has been initialized.
+        formData[this.name] = formData[this.name] || [];
 
-    console.log(formData);
+        formData[this.name].push(this.value);
+      }
+
+      return;
+    }
+
+    // Else, for the majority of input types, just map formData.name = formData.value.
+    //
+    // Using jQuery's .val() here to use the same code for regular inputs, selects, and
+    //  selected radio button inputs.
+    formData[this.name] = $(this).val();
+  });
+
+  console.log(formData);
 });
